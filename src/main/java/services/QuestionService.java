88e3069b4ce7f6abd0f2166dc7c96e1aaa510ac7
Alejandro Ballesteros <alexbalmar@gmail.com>
2016-02-03 18:15:57 +0100

    Crear Votos
    Creados los metodos de servicios, nuevo parametro a question y sus
    respectivos geters y seters

--- src/main/java/services/QuestionService.java
+++ src/main/java/services/QuestionService.java
@@ -3,6 +3,7 @@ package services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import repositories.QuestionRepository;
 import domain.DomainEntity;
@@ -16,8 +17,29 @@ public class QuestionService extends DomainEntity{
 	private QuestionRepository questionRepository;
 
 	//Methods
-	public Question create(){
+	public Question create(Integer surveyId){
 		Question o = new Question();
+		o.setSurveyId(surveyId);
+		questionRepository.saveAndFlush(o);
 		return o;
+	}
+
+	public Question create(String question) {
+		Question o = new Question();
+		o.setText(question);
+		
+		questionRepository.saveAndFlush(o);
+		return o;
+	}
+
+	public int saveAndFlush(Question question) {
+		Assert.notNull(question);
+		Question q2 = questionRepository.saveAndFlush(question);
+		int questionID = q2.getId();
+		return questionID;
+	}
+
+	public Question findOne(int questionId) {
+		return questionRepository.findOne(questionId);
 	}
 }
